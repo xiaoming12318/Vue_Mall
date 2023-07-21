8eab3f2ca56bcf1eb684b24cf1aea25930f7e1fd
Jacob <975409698@qq.com>
2023-07-21 11:27:39 +0800

    登录的基础应用

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,5 +1,7 @@
 //axios基础的封装
 import axios from 'axios'
+import 'element-plus/theme-chalk/el-message.css'
+import {ElMessage} from 'element-plus'
 
 const httpInstance=axios.create({
     //根域名，请求这个路径
@@ -14,6 +16,11 @@ httpInstance.interceptors.request.use(config=>{
 },e=>Promise.reject(e))
 //axios响应拦截器
 httpInstance.interceptors.response.use(res=>res.data,e=>{
+    //统一错误提示
+    ElMessage({
+        type:'warning',
+        message:e.response.data.message
+    })
     return Promise.reject(e)
 })
 
--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,5 +1,9 @@
 <script setup>
 import {ref,onMounted} from 'vue'
+import {loginAPI} from '@/apis/user'
+import 'element-plus/theme-chalk/el-message.css'
+import {ElMessage} from 'element-plus'
+import {useRouter} from 'vue-router'
 //表单校验（账户名+密码）
 
 //1.准备表单对象
@@ -32,7 +36,27 @@ const rules={
         }
     ]
 }
-
+//获取form实例做统一校验
+const formRef=ref(null)
+const router=useRouter()
+const doLogin=()=>{
+    const {account,password}=form.value
+    //调用实例方法
+    formRef.value.validate(async(valid)=>{
+        //valid代表表单所有都通过校验 才为true
+        //以valid作为判断条件，如果通过校验才执行登录逻辑
+        if(valid){
+            //xiaotuxian001
+            //TODO LOGIN
+            const res=await loginAPI({account,password})
+            console.log(res);
+            //1.提示用户
+            ElMessage({type:'success',message:'登录成功'})
+            //2.跳转首页
+            router.replace({path:'/'})
+        }
+    })
+}
 
 </script>
 
@@ -58,7 +82,7 @@ const rules={
         </nav>
         <div class="account-box">
           <div class="form">
-            <el-form :model="form" :rules="rules" label-position="right" label-width="60px"
+            <el-form ref="formRef" :model="form" :rules="rules" label-position="right" label-width="60px"
               status-icon>
               <el-form-item prop="account"  label="账户">
                 <el-input v-model="form.account"/>
@@ -71,7 +95,7 @@ const rules={
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
-              <el-button size="large" class="subBtn">点击登录</el-button>
+              <el-button size="large" class="subBtn" @click="doLogin">点击登录</el-button>
             </el-form>
           </div>
         </div>
